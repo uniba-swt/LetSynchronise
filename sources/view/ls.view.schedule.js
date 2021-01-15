adda5e76586742089b35a3d361ac883f984ecfe2
eyip002 <eyip002@aucklanduni.ac.nz>
2021-01-15 19:14:24 +0100

    Added UI hooks for task dependencies and analysis

--- sources/view/ls.view.schedule.js
+++ sources/view/ls.view.schedule.js
@@ -1,10 +1,32 @@
 'use strict';
 
 class ViewSchedule {
-    constructor() {
-        this.root = document.querySelector('#view-schedule');
-    }
+    root = null;
     
+    updateButton = null;
+    
+    constructor() {
+        this.root = document.querySelector('#nav-analyse');
+        
+        this.updateButton = this.root.querySelector('#update');
+        
+        this.schedule = d3.select('#view-schedule');
+        
+        this.setupUpdateButtonListener();
+    }
+
+    // -----------------------------------------------------
+    // Setup listeners
+    
+    setupUpdateButtonListener() {
+        this.updateButton.addEventListener('click', event => {
+            // Prevent the default behaviour of submitting the form and the reloading of the webpage.
+            event.preventDefault();
+            
+            this.schedule.text('Schedule to be added.');
+        });
+    }
+
     toString() {
         return "ViewSchedule";
     }
