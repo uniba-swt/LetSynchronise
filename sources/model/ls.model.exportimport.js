90c2d893be3ba7506a7b0ff9614534177ea06457
eyip002 <eyip002@aucklanduni.ac.nz>
2021-03-31 18:49:38 +0200

    Simplified the refreshing of views by the models and when importing

--- sources/model/ls.model.exportimport.js
+++ sources/model/ls.model.exportimport.js
@@ -1,32 +1,12 @@
 'use strict';
 
 class ModelExportImport {
-//    updateTasks = null;                 // Callback to function in ls.view.task
-//    updateDependencySelectors = null;   // Callback to function in ls.view.dependency
-//    updateConstraintSelectors = null;   // Callback to function in ls.view.constraint
-
     database = null;
     modelTask = null;
     modelDependency = null;
     
     constructor() { }
     
-    
-    // -----------------------------------------------------
-    // Registration of callbacks from the controller
-/*
-    registerUpdateTasksCallback(callback) {
-        this.updateTasks = callback;
-    }
-    
-    registerUpdateDependencySelectorsCallback(callback) {
-        this.updateDependencySelectors = callback;
-    }
-    
-    registerUpdateConstraintSelectorsCallback(callback) {
-        this.updateConstraintSelectors = callback;
-    }
-    */
     
     // -----------------------------------------------------
     // Registration of models
