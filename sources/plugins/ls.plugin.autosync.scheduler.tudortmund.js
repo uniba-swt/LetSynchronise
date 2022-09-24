a5775cbfd632bb365b1a55841841d920e6ad50eb
eyip002 <eyip002@aucklanduni.ac.nz>
2022-09-24 15:19:19 +0200

    Improved http error handling

--- sources/plugins/ls.plugin.autosync.scheduler.tudortmund.js
+++ sources/plugins/ls.plugin.autosync.scheduler.tudortmund.js
@@ -19,7 +19,6 @@ class PluginAutoSyncSchedulerTuDortmund {
         // Schedule the LET system with an external web tool
         const computedSchedule = await this.Algorithm(system, executionTiming);
         if (computedSchedule == null) {
-            alert('Plugin does not support LET tasks with non-zero initial offsets');
             return
         }
         
@@ -33,17 +32,22 @@ class PluginAutoSyncSchedulerTuDortmund {
     // Trigger an external scheduling tool
     // TODO: Make use of executionTiming when instantiating task execution times
     static async Algorithm(system, executionTiming) {
-        const response = await fetch('http://localhost:8080/', {
+        const url = 'http://localhost:8080/';
+        return fetch(url, {
             method: 'POST',
             body: JSON.stringify(system),
             headers: { 'Content-Type': 'application/json' }
+        }).then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                alert('Plugin does not support LET tasks with non-zero activation offsets!');
+                return null;
+            }
+        }).catch(error => {
+            alert(`${url} could not be reached!`)
+            return null;
         });
-        
-        const status = await response.status;
-        if (status == 200) {
-            return response.json();
-        }
-        return null;
     }
     
 }
