dceaaafab749a4ff17daf2215bdd1adf1616a159
eyip002 <eyip002@aucklanduni.ac.nz>
2022-09-24 15:42:26 +0200

    Updated comments

--- sources/plugins/ls.plugin.autosync.scheduler.tudortmund.js
+++ sources/plugins/ls.plugin.autosync.scheduler.tudortmund.js
@@ -7,30 +7,30 @@ class PluginAutoSyncSchedulerTuDortmund {
     static get Category() { return PluginAutoSync.Category.Scheduler; }
 
     
-    // Uses an external web tool to schedule the task executions
+    // Uses an external web tool to schedule the task executions.
     static async Result(makespan, executionTiming) {
-        // Delete existing schedule
+        // Delete existing schedule.
         await PluginAutoSync.DeleteSchedule();
         
-        // Retrieve the LET system
+        // Retrieve the LET system.
         const systemElementSelected = ['inputs','outputs','tasks','dependencies','eventChains','constraints'];
         const system = await PluginAutoSync.DatabaseContentsGet(systemElementSelected);
         
-        // Schedule the LET system with an external web tool
+        // Schedule the LET system with an external web tool.
         const computedSchedule = await this.Algorithm(system, executionTiming);
         if (computedSchedule == null) {
             return
         }
         
-        // Save the externally computed task schedule and compute the dependency and event chain instances
+        // Save the externally computed task schedule and compute the dependency and event chain instances.
         const scheduleElementSelected = ['schedule'];
         return PluginAutoSync.DatabaseContentsDelete(scheduleElementSelected)
             .then(result => PluginAutoSync.DatabaseContentsSet(computedSchedule, scheduleElementSelected))
             .then(result => PluginAutoSync.CreateAllDependencyAndEventChainInstances(makespan));
     }
 
-    // Trigger an external scheduling tool
-    // TODO: Make use of executionTiming when instantiating task execution times
+    // Trigger an external scheduling tool.
+    // TODO: Make use of executionTiming when instantiating task execution times.
     static async Algorithm(system, executionTiming) {
         const url = 'http://localhost:8080/';
         return fetch(url, {
